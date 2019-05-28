ab0405a891d0c7b1f37ec15b84c27ec02092109e
Front-End Student <frontendprogramming@2ndchancecenter.com>
2019-05-27 20:01:25 -0700

    need to fix timer for images

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,13 +1,60 @@
 "use strict";
 
+// Navigation buttons
 var btn = document.querySelectorAll("[data-toggle='tab']");
 
-// IE11 does not like foreach function
+// Images for auto change
+var afterImages = document.getElementsByClassName("after");
+
+// F.Y.I. IE11 does not like foreach function
+// Click event for navigation buttons
 for (let index = 0; index < btn.length; index++) {
     const element = btn[index];
     element.addEventListener("click", removeActive);
 }
 
+// After images mouse event handlers
+for (let index = 0; index < afterImages.length; index++) {
+    const element = afterImages[index];
+    element.addEventListener("mouseenter", togglelock);
+    element.addEventListener("mouseleave", togglelock);
+
+    element.addEventListener("mouseenter", changeOpacity);
+    element.addEventListener("mouseleave", changeOpacity);
+}
+
+function togglelock(){
+
+    if (!this.parentElement.classList.contains("lock")) {
+        this.parentElement.classList.add("lock");
+        this.style.opacity = 0;
+        this.parentElement.children[0].classList.remove("collapse");
+    }
+    else{
+        this.parentElement.classList.remove("lock");
+        this.style.opacity = 1;
+        // this.parentElement.children[0].classList.add("collapse");
+    }
+
+}
+
+
+function changeOpacity() {
+    if (!this.parentElement.classList.contains("lock")) {
+
+        if (this.style.opacity == 1 || this.style.opacity == "") {
+            this.parentElement.children[0].classList.remove("collapse");
+            this.style.opacity = 0;
+
+        }
+        else {
+            this.style.opacity = 1;
+            this.parentElement.children[0].classList.add("collapse");
+        }
+    }
+}
+
+// Navigation click event handler. Removes active class
 function removeActive(e) {
     var sibling = e.target.parentElement.children;
     
@@ -17,54 +64,38 @@ function removeActive(e) {
     }
 }
 
-var afterImages = document.getElementsByClassName("after");
+var timer1 = window.setInterval(imageopacity, 4000);
 
-var timer = window.setInterval(imageopacity, 4000);
 
+// Timer function for image switch
 function imageopacity() {
 
     for (let index = 0; index < afterImages.length; index++) {
         const element = afterImages[index];
-        if (element.style.opacity == 1 || element.style.opacity == "") {
-            element.style.opacity = 0;
-        } else {
-            element.style.opacity = 1;
+
+        if (!element.parentElement.classList.contains("lock")) {
+            if (index % 2 != 0) {
+                if (element.style.opacity == 1 || element.style.opacity == "") {
+                    element.parentElement.children[0].classList.remove("collapse");
+                    element.style.opacity = 0;
+                } else {
+                    element.parentElement.children[0].classList.add("collapse");
+                    element.style.opacity = 1;
+                }
+            }
+            else {
+                // Even images are delayed 4 seconds. Giving the appearance of alternation
+                // window.setTimeout(function () {
+                    if (element.style.opacity == 1 || element.style.opacity == "") {
+                        element.parentElement.children[0].classList.remove("collapse");
+                        element.style.opacity = 0;
+                    } else {
+                        element.parentElement.children[0].classList.add("collapse");
+                        element.style.opacity = 1;
+                    }
+                // }, 1000);
+            }
         }
     }
-
 }
-
-
-// var img = document.querySelectorAll(".card-img-overlay");
-
-// for (let index = 0; index < img.length; index++) {
-//     const element = img[index];
-//     element.addEventListener("mouseenter", showBeforePicture);
-//     element.addEventListener("mouseout", showAfterPicture);
-// }
-
-// function showBeforePicture(e){
-//     var pictureUrl = e.target.nextElementSibling.src.split("/");
-//     var beforePictureName = pictureUrl[5].split(".")[0] + "-before.jpg";
-//     var beforePictureUrl = "";
-    
-//     for (let index = 0; index < pictureUrl.length-1; index++) {
-//         const section = pictureUrl[index];
-//         beforePictureUrl = beforePictureUrl + section + "/";
-//     }
-    
-//     beforePictureUrl = beforePictureUrl + beforePictureName;
-    
-//     e.target.nextElementSibling.src = beforePictureUrl;
-// }
-
-// function showAfterPicture(e){
-//     var pictureUrl = e.target.nextElementSibling.src.split("-before");
-
-//     var beforePictureUrl = "";
-    
-//     beforePictureUrl = pictureUrl[0] + pictureUrl[1];
-        
-//     e.target.nextElementSibling.src = beforePictureUrl;
-// }
 
