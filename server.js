bb0432d5a385f4b29f3a7570429ee122869a4f2d
Phong D. Nguyen <phongnguyenprograms@gmail.com>
2020-09-02 16:04:07 -0700

    Configure for deployment

--- server.js
+++ server.js
@@ -3,8 +3,10 @@
 const port = process.env.PORT || 5000;
 
 let express = require('express');
+let path = require('path');
 
 let app = express();
+process.env.PWD = process.cwd();
 
-app.use(express.static([__dirname, 'src'].join('\\')));
+app.use(express.static(path.join(process.env.PWD, 'src')));
 app.listen(port);
